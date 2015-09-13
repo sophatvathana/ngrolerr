e91aabd58dc0f8c9c05bf5f95afd10b1130d94ab
sovathana-phat <sovathana.phat@gmail.com>
2015-09-13 12:35:59 +0700

    library

---
+++ index.js
@@ -0,0 +1,14 @@
+/*====+++++========+++++===* 
+* @Author: sovathana
+* @Date:   2015-09-13 12:33:13
+* @Last Modified by:   sovathana
+* @Last Modified time: 2015-09-13 12:33:50
+* @Email: sovathana.phat@gmail.com
+* @Facebook && Twitter : Sophatvathana
+* @Project: Quick-q
+* @FileName: index.js
+*==========================
+*/
+
+'use strict';
+module.exports = require('./lib/ngRolerr');
