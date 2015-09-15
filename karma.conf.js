54d0a28ed4e86700c48616e8c3e82520d6d1170f
sovathana-phat <sovathana.phat@gmail.com>
2015-09-16 03:43:55 +0700

    travis

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'tests/bundles/angular/angular.js',
-      'tests/bundles/angular/angular-cookies.js',
+      'tests/bundles/angular/angular-mocks.js',
       'lib/*.js',
       'tests/*Spec.js'
     ],
